71328627a9e2c32baa5f4eb564eb11582ba7e464
ohseongmin <ohseongmin0614@gmail.com>
2024-12-18 03:54:26 +0900

    Feat:DetailPage 추가,Footer 바닥고정

--- src/components/layout-page/Footer.jsx
+++ src/components/layout-page/Footer.jsx
@@ -1,5 +1,20 @@
 const Footer = () => {
-  return <footer>&copy; 2024 Team0910. All rights reserved.</footer>;
+  return (
+    <footer
+      style={{
+        position: 'fixed',
+        bottom: 0,
+        left: 0,
+        width: '100%',
+        background: '#f8f8f8',
+        textAlign: 'left',
+        padding: '10px 0',
+        borderTop: '1px solid #ddd',
+      }}
+    >
+      &copy; TeamSupuerCoding10. All rights reserved.
+    </footer>
+  );
 };
 
 export default Footer;
